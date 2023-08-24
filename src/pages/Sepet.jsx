9fa6386857393769f844fdf90a21a3ff5e1ba8a7
= <=>
2023-08-24 11:06:45 +0300

    sepet ekleme çıkarma işlemi

--- src/pages/Sepet.jsx
+++ src/pages/Sepet.jsx
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Fav({ user }) {
-  const [favs, setFavs] = useState([]);
+export default function Sepet({ user }) {
+  const [sepets, setSepets] = useState([]);
 
   useEffect(() => {
-    setFavs(JSON.parse(localStorage.getItem("favs")) ?? []);
+    setSepets(JSON.parse(localStorage.getItem("sepets")) ?? []);
   }, []);
 
   // Favori ürünü çıkarma işlemi
-  const removeFromFavs = (id) => {
-    const updatedFavs = favs.filter((item) => item.id !== id);
-    setFavs(updatedFavs);
-    localStorage.setItem("favs", JSON.stringify(updatedFavs));
+  const removeFromSepets = (id) => {
+    const updatedSepets = sepets.filter((item) => item.id !== id);
+    setSepets(updatedSepets);
+    localStorage.setItem("sepets", JSON.stringify(updatedSepets));
   };
 
   return (
     <>
-      <h1>Favoriler</h1>
+      <h1>Sepet</h1>
 
-      <p>Toplam {favs.length} ürün favorilere eklenmiştir.</p>
+      <p>Toplam {sepets.length} ürün sepete eklenmiştir.</p>
 
       <div className="list-group">
-        {favs.map((item) => (
+        {sepets.map((item) => (
           <div key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
             <Link to={`/products/product/${item.id}`} className="text-decoration-none">
               {item.title.toUpperCase()}
             </Link>
-            <button className="btn btn-danger" onClick={() => removeFromFavs(item.id)}>
-              Favorilerden Çıkar
+            <button className="btn btn-danger" onClick={() => removeFromSepets(item.id)}>
+              Sepetten  Çıkar
             </button>
           </div>
         ))}
